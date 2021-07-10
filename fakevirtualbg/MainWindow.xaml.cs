aaf82e7b1f99f5a4ca9152056cff82bc2bcf04ae
Matthias Wang <matthiaswang60@gmail.com>
2021-07-10 19:17:35 +0800

    init

---
+++ fakevirtualbg/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fakevirtualbg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OpenVideoBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //init an openfiledialog and get the filepath of the video
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Video files (*.mp4;*.mov)|*.mp4;*.mov";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filepathText.Text = openFileDialog.FileName;
+            }
+                
+        }
+    }
+}
