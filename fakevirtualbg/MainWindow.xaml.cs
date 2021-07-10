eefc5529c512262377d38314cdbff96184f4b438
Matthias Wang <matthiaswang60@gmail.com>
2021-07-10 21:08:29 +0800

    added ability to reverse video

--- fakevirtualbg/MainWindow.xaml.cs
+++ fakevirtualbg/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,6 +36,28 @@ namespace fakevirtualbg
                 filepathText.Text = openFileDialog.FileName;
             }
                 
+        }
+
+        private void VideoSaveLoc_Click(object sender, RoutedEventArgs e)
+        {
+            //get the directory to save the video by using an openfolderdialog
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+                savepathText.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void GenerateVidBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //first, reverse the video
+            Process p = new Process();
+            p.StartInfo.FileName = @"Resources\ffmpeg.exe";
+            p.StartInfo.Arguments = "-i " + "\"" + filepathText.Text + "\"" + " -vf reverse " + "\"" + System.IO.Path.Combine
+                (savepathText.Text, "output" + System.IO.Path.GetExtension(filepathText.Text)) + "\"";
+
+            MessageBox.Show(p.StartInfo.Arguments.ToString());
+            p.Start();
         }
     }
 }
